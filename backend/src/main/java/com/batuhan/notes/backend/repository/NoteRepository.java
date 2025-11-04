0abd20a0d024804244dff1eb5fdfa550d5ed5e79
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-04 17:08:32 +0300

    Create NoteRepository.java

---
+++ backend/src/main/java/com/batuhan/notes/backend/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package com.batuhan.notes.backend.repository;
+
+import com.batuhan.notes.backend.entity.NoteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<NoteEntity, Long> {
+
+}
