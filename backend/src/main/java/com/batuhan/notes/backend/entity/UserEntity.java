75554579277f9a9625ab20445d5dd9341b06cd91
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-07 17:45:24 +0300

    Update UserEntity.java

--- backend/src/main/java/com/batuhan/notes/backend/entity/UserEntity.java
+++ backend/src/main/java/com/batuhan/notes/backend/entity/UserEntity.java
@@ -1,16 +1,15 @@
 package com.batuhan.notes.backend.entity;
 
+import com.batuhan.notes.backend.user.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name = "_user")
@@ -30,9 +29,12 @@ public class UserEntity implements UserDetails {
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        return role.getAuthorities();
     }
 
     @Override
