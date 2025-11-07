1b471638b5e525f88924ea0c459de74281a18cb6
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-07 17:47:54 +0300

    Create AuthenticationController.java

---
+++ backend/src/main/java/com/batuhan/notes/backend/controller/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.batuhan.notes.backend.controller;
+
+import com.batuhan.notes.backend.dto.AuthenticationRequest;
+import com.batuhan.notes.backend.dto.AuthenticationResponse;
+import com.batuhan.notes.backend.dto.RegisterRequest;
+import com.batuhan.notes.backend.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+}
