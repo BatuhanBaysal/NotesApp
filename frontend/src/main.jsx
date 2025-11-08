5c22bf23649de146a93850ab42c5e9130ff39b1e
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-08 11:17:04 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom'; 
+import { AuthProvider } from './context/AuthContext'; 
+import { ThemeProvider } from './context/ThemeContext.jsx';
+import axios from 'axios';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+axios.defaults.xsrfCookieName = null;
+axios.defaults.xsrfHeaderName = null;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <ThemeProvider>
+                <AuthProvider> 
+                    <App />
+                    <ToastContainer 
+                        position="top-right" 
+                        autoClose={5000}
+                        hideProgressBar={false}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        pauseOnFocusLoss
+                        draggable
+                        pauseOnHover
+                        theme="colored" 
+                    />
+                </AuthProvider>
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+);
