a7aa1c58b2373cb997623282f977f13b9b759130
Batuhan Baysal <batuhanbaysal3@gmail.com>
2025-11-09 11:10:18 +0300

    Create ThemeContext.jsx

---
+++ frontend/src/context/ThemeContext.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const ThemeContext = createContext();
+
+const getInitialTheme = () => {
+    if (typeof window !== 'undefined' && window.localStorage) {
+        const storedPrefs = window.localStorage.getItem('theme');
+        if (typeof storedPrefs === 'string') {
+            return storedPrefs;
+        }
+        const userMedia = window.matchMedia('(prefers-color-scheme: dark)');
+        if (userMedia.matches) {
+            return 'dark';
+        }
+    }
+    return 'light';
+};
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState(getInitialTheme);
+
+    const toggleTheme = () => {
+        setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+    };
+
+    useEffect(() => {
+        const root = window.document.documentElement;
+        root.classList.remove(theme === 'dark' ? 'light' : 'dark');
+        root.classList.add(theme);
+        localStorage.setItem('theme', theme);
+        
+    }, [theme]); 
+    
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
